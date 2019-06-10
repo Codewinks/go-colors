df6894881e0399446732c6a463f9a73fbaa4b922
Kristopher Chun <kristopher.chun@dataq.ai>
2019-06-09 18:03:46 -0700

    Updated example.

--- example/colors.go
+++ example/colors.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codewinks/go-colors"
 )
 
 func main() {
 	colors.ShowExample()
+
+	fmt.Println(colors.Green("Prints text in green."))
+	fmt.Printf("Prints %s in blue.\n", colors.Blue("text"))
+
+	fmt.Printf("Different colors such as %s, %s, %s, and %s.\n", colors.Blue("blue"), colors.Green("green"), colors.Red("red"), colors.Yellow("yellow"))
 }
